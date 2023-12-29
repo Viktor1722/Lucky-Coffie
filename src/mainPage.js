bcfc3a787ece14b86bf1badd0b5a086952dc6d9f
viktor.velizarov <viktor.velizarov@bycape.io>
2023-12-30 00:39:07 +0200

    added social media sharing

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Button} from '@mui/material';
+import { Container, Box} from '@mui/material';
 
 const wishes = [
     "Да бъдеш здрав и щастлив всеки ден на живота си!",
@@ -29,8 +29,38 @@ function MainPage () {
         <br/><br/>
         <h3><p aria-placeholder="И твоят късмет е...." className="fortune-display">{fortune}</p></h3>
         <br/><br/>
-        <Button sx={{backgroundColor: "#ffff" ,color: "#000000", borderRadius: "210px"}} disableElevation={true} className="form-submit-button" variant="contained">Към книгата с отзиви</Button>
+        {/* <Button disableElevation={true} className="form-submit-button" variant="contained">Към книгата с отзиви</Button> */}
+        <Box className="links-to-socialmedia">
+            <Box className="links-box">
+                <a href="https://icons.getbootstrap.com/icons/facebook/" className="facebook-submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 
+                        3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 
+                        1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 
+                        1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+                    </svg>
+                </a>
 
+                <a href="https://icons.getbootstrap.com/icons/facebook/" className="facebook-submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 
+                        3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 
+                        1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 
+                        1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+                    </svg>
+                </a>
+
+                <a href="https://icons.getbootstrap.com/icons/facebook/" className="facebook-submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 
+                        3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 
+                        1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 
+                        1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+                    </svg>
+                </a>
+            </Box>
+            <button className="submit-button">Към книгата с отзови</button>
+        </Box>
     </Container>
     )
 }
